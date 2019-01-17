321a029c43dbcf39587cd1671fe39d5aca4d30ac
Leandro Carvalho <skateforever@users.noreply.github.com>
2019-01-17 10:01:56 -0200

    Update backup_zimbra.sh
    Script updated to make it easy to retrieve settings and information from server users

--- email/backup_zimbra.sh
+++ email/backup_zimbra.sh
@@ -1,87 +1,94 @@
 #!/bin/bash
 
-################################################################################################################
-#                                                                                                              #
-# Autor: Leandro Carvalho <carvalho.inacio@gmail.com>                                                          #
-# Versao 20150413                                                                                              #
-#                                                                                                              #
-# Secretaria de Saude do Estado de Alagoas                                                                     #
-# Automatizacao de Servicos de backup                                                                          #
-# Limpa os logs com mais de 7 dias                                                                             #
-# Toda a acao sera informada por email                                                                         #
-#                                                                                                              #
-################################################################################################################
+DATA=$(date +%Y%m%d)
+YESTERDAY=$(date -d "1 day ago" '+%d%m%Y')
+TWODAYSAGO=$(date -d "2 day ago" '+%m/%d/%Y')
+TODAY=$(date '+%m/%d/%Y')
+DAY_OF_WEEK=$(date +%a)
 
-HR=$(date +%H)
-DATA=$(date +%Y%m%d-%H%M)
-SUN=$(date +%a)
-
-RSYNC_LOG=/var/log/zimbra_rsync-${DATA}.log
-LOCK=/tmp/check_spam.lock
-
-DTSMAIL=(emails@dominio.com)
+BACKUP_MNT=/mnt/backup/zimbra
+BACKUP_CONFIG=${BACKUP_MNT}/config-${DATA}
+BACKUP_FULLDIR=${BACKUP_MNT}/full-${DATA}
+BACKUP_INCDIR=${BACKUP_MNT}/inc-${DATA}
+BACKUP_LOG=/var/log/zimbra_backup-${DATA}.log
 
 (
-if [ -f ${LOCK} ]; then
-    echo "Arquivo de lock encontrado, favor verificar o que aconteceu." >> ${RSYNC_LOG}
-    exit 0
-else
-    echo "${DATA}"
-    echo "Criando o arquivo de lock..."
-    touch ${LOCK}
-    mount | grep "media/backup"
-    if [ $? != 0 ]; then
-        mount /dev/disk/by-id/"UUID_DO_DISCO" /mnt/backup
-        if [ $? != 0 ]; then
-            echo "Nao foi possivel montar o HD!"
-            RESULTADO="FALHOU"
-            exit 1
-        else
-            # verificando se consegue ler o disco
-	    # se conseguir realiza o backup
-            ls /mnt/backup > /dev/null 2>&1
-            if [ $? == 0 ]; then
-                echo "Parando o zimbra" 
-                su - zimbra -c "zmcontrol stop"
-                if [ $? == 0 ]; then
-                    echo "Inicio da sincronizacao"
-                    rsync -azt --delete --progress --stats /opt/zimbra/ /mnt/backup/zimbra
-                    if [ $? == 0 ]; then
-                        echo "Fim da sincronizacao!"
-                        echo "Backup Realizado com Sucesso!"
-                        echo "Iniciando o zimbra!"
-                        su - zimbra -c "zmcontrol start"
-                        if [ $? != 0 ]; then
-                            RESULTADO="FALHOU"
-                            echo "Falha ao iniciar o zimbra, favor verificar o motivo!"
-                            exit 1 
-                        else
-                            RESULTADO="OK"
-                        fi
-                    fi
-                else
-                     echo "Nao foi possivel realizar o backup do zimbra!"
-                     RESULTADO="FALHOU"
-                     exit 1
-                fi
-            else
-                echo "Nao foi possivel fazer o backup!"
-                echo "Possivel erro de entrada e saida"
-                exit 1
-            fi
-        fi
-    else
-        echo "Nao foi possivel parar o zimbra para realizar o backup"
-    fi
+echo "$(date "+%H:%M") Realizando backup das configuracoes..."
+if [ ! -d ${BACKUP_CONFIG} ]; then
+    su - zimbra -c "mkdir -pv ${BACKUP_CONFIG}"
 fi
-if [ ! -d /mnt/backup/scripts ]; then
-    mkdir -p /mnt/backup/scripts
-else
-    cp -av /usr/local/sbin/* /mnt/backup/scripts/
-fi
-rm -f ${LOCK}
-umount /mnt/backup
-) > ${RSYNC_LOG} 2>&1
-cat ${RSYNC_LOG} | strings | mail -s "Backup Zimbra ${RESULTADO} - ${DATA}" ${DTSMAIL[@]}
 
-find /var/log/ -maxdepth 1 -mtime +7 -name 'zimbra_rsync-*.log' -print | xargs rm -f
+echo "$(date "+%H:%M") Listando os dominios..."
+su - zimbra -c "zmprov gad > ${BACKUP_CONFIG}/domains.txt"
+
+echo "$(date "+%H:%M") Listando as contas de admin..."
+su - zimbra -c "zmprov gaaa > ${BACKUP_CONFIG}/admins.txt"
+
+echo "$(date "+%H:%M") Realizando backup das listas de distribuicao..."
+if [ ! -d ${BACKUP_CONFIG}/listas ]; then
+    su - zimbra -c "mkdir -pv ${BACKUP_CONFIG}/listas"
+fi
+
+echo "$(date "+%H:%M") Listando todas as listas de distribuicao..."
+su - zimbra -c "zmprov gadl > ${BACKUP_CONFIG}/listas/00_listas_distribuicao.txt"
+
+echo "$(date "+%H:%M") Listando todos os membros das listas de distribuicao..."
+for LIST in $(su - zimbra -c "zmprov gadl"); do
+    su - zimbra -c "zmprov gdlm ${LIST} > ${BACKUP_CONFIG}/listas/${LIST}_membros.txt"
+done
+
+echo "$(date "+%H:%M") Limpando listas vazias..."
+find ${BACKUP_CONFIG}/listas/ -type f -empty -exec rm -f {} \+
+
+echo "$(date "+%H:%M") Realizando backup dos alias..."
+if [ ! -d ${BACKUP_CONFIG}/aliases/ ]; then
+    su - zimbra -c "mkdir -pv ${BACKUP_CONFIG}/aliases"
+fi
+
+for USER in $(su - zimbra -c "zmprov -l gaa | sort | grep -vE \"^spam|^virus|^hal|^galsync|^zmbackup|^ldap\""); do
+    su - zimbra -c "zmprov ga ${USER} | grep zimbraMailAlias" | awk '{print $2}' > ${BACKUP_CONFIG}/aliases/${USER}.txt
+done
+
+echo "$(date "+%H:%M") Limpando aliases vazios..."
+find ${BACKUP_CONFIG}/aliases/ -type f -empty -exec rm -f {} \+
+
+echo "$(date "+%H:%M") Listando nome e senha dos usuários..."
+for USER in $(su - zimbra -c "zmprov -l gaa | sort | grep -vE \"^spam|^virus|^hal|^galsync|^zmbackup|^ldap\""); do
+    SENHA=$(su - zimbra -c "zmprov -l ga ${USER} userPassword" | grep userPassword: | awk '{ print $2}')
+    DISPLAY_NAME=$(su - zimbra -c "zmprov ga ${USER}" | grep -E ^displayName | cut -d " " -f 2-)
+    su - zimbra -c "echo \"${USER}:${SENHA}:${DISPLAY_NAME}\"" >> ${BACKUP_CONFIG}/users.txt
+done
+
+if [ "${DAY_OF_WEEK}" == "Sun" ]; then
+    echo "$(date "+%H:%M") Realizando o backup full das contas..."
+    if [ ! -d ${BACKUP_FULLDIR} ]; then
+        su - zimbra -c "mkdir -pv ${BACKUP_FULLDIR}"
+    fi
+
+    for USER in $(su - zimbra -c "zmprov -l gaa | sort | grep -vE \"^spam|^virus|^hal|^galsync|^zmbackup|^ldap\""); do
+        su - zimbra -c "zmmailbox -z -m ${USER} getRestURL '/?fmt=tgz' > ${BACKUP_FULLDIR}/${USER}-full-${DATA}.tgz" 2> /dev/null
+        sleep 2
+    done
+
+    echo "$(date "+%H:%M") Limpando backups vazios..."
+    find ${BACKUP_FULLDIR}/ -type f -empty -exec rm -f {} \+
+else
+    echo "$(date "+%H:%M") Realizando o backup incremental das contas..."
+    if [ ! -d ${BACKUP_INCDIR} ]; then
+        su - zimbra -c "mkdir -pv ${BACKUP_INCDIR}"
+    fi
+
+    for USER in $(su - zimbra -c "zmprov -l gaa | sort | grep -vE \"^spam|^virus|^hal|^galsync|^zmbackup|^ldap\""); do
+        su - zimbra -c "zmmailbox -z -m ${USER} getRestURL '/?fmt=tgz&query=after:\"${TWODAYSAGO}\" and before:\"${TODAY}\"' > ${BACKUP_INCDIR}/${USER}-${YESTERDAY}.tgz" 2> /dev/null
+        sleep 2
+    done
+
+    echo "$(date "+%H:%M") Limpando backups vazios..."
+    find ${BACKUP_INCDIR}/ -type f -empty -exec rm -f {} \+
+fi
+
+echo "Backup finalizado as $(date "+%H:%M")!"
+) >> ${BACKUP_LOG}
+
+find /mnt/backup/zimbra/ -mtime +8 -type d -execdir rm -rf {} \+
+find /var/log/zimbra_backup-* -mtime +7 -type f -exec rm -rf {} \+
