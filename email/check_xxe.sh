a8d30601d27fa1f4b205493defeffb9148ea97bc
Leandro Carvalho <skateforever@users.noreply.github.com>
2019-05-08 10:15:08 -0300

    Update check_xxe.sh

--- email/check_xxe.sh
+++ email/check_xxe.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+IP_INT=""
+IP_EXT=""
+
 data_LOG=$(date +%Y-%m-%d)
 access_LOG=/opt/zimbra/log/access_log.${data_LOG}
 
-for IP in $(awk '{print $1}' ${access_LOG} | grep -Ev "192.168." | grep -Ev "10.84." | grep -Ev "127.0.0.1" | sort -u | sed 's/,$//' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"); do
+for IP in $(awk '{print $1}' ${access_LOG} | grep -Ev "127.0.0.1" | grep -v ${IP_INT} | grep -v ${IP_EXT} | sort -u | sed 's/,$//' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"); do
     PAIS=$(geoiplookup ${IP} | awk '{print $5}')
     if [[ "${PAIS}" != "Brazil" ]]; then
         RESTANTES=$(grep ${IP} ${access_LOG} | awk '{print $1}' | sort -u | sed 's/,$//' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
