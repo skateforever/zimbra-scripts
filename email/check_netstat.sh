58e4a5be2b123ec05ded12d21bfb8f61fc69a469
Leandro Carvalho <skateforever@users.noreply.github.com>
2019-05-08 10:28:31 -0300

    Update check_netstat.sh

--- email/check_netstat.sh
+++ email/check_netstat.sh
@@ -8,7 +8,7 @@ for IP in $(netstat -napult | grep -v "Conex" | grep -v "Remoto" | awk '{print $
     if [[ "${PAIS}" != "Brazil" ]]; then
         PORTA_DESTINO=$(netstat -npulta | grep ${IP} | awk '{print $5}' | cut -d ":" -f 2)
         kill_PROCESSO=$(netstat -npulta | grep ${IP} | awk '{print $7}' | grep -v "-" | cut -d "/" -f 1)
-        if (( $PORTA_DESTINO == 80 )) || (( $PORTA_DESTINO == 8080 )) || (( $PORTA_DESTINO == 443 )) || (( $PORTA_DESTINO == 8443 )) || (( $PORTA_DESTINO == 1337 )) || (( $PORTA_DESTINO == 4444 )); then
+        if [[ $PORTA_DESTINO == 80 ]] || [[ $PORTA_DESTINO == 8080 ]] || [[ $PORTA_DESTINO == 443 ]] || [[ $PORTA_DESTINO == 8443 ]] || [[ $PORTA_DESTINO == 1337 ]] || [[ $PORTA_DESTINO == 4444 ]]; then
             if [[ ! $(grep ${IP} ${BLOCKIP_FILE}) ]]; then
                 iptables -I INPUT -s ${IP} -j DROP
                 iptables -I OUTPUT -d ${IP} -j DROP
